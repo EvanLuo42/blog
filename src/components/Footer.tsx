d4c91d0141ad1b06617c82426be7b9b4e3f32c71
Evan Luo <knacks.insects_0o@icloud.com>
2024-05-30 11:10:20 +0800

    feat: Add source code link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <>
       <Separator></Separator>
       <div className="bottom-0 flex justify-center items-center p-4">
-        <p>©Copyright EvanLuo42. All rights reserved.</p>
+        <p>©Copyright EvanLuo42. All rights reserved. Here is the <a className="underline" href="https://github.com/EvanLuo42/blog">source code</a>.</p>
       </div>
     </>
   )
